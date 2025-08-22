b829dde7cd53646db0e233bcea35476adf284051
Mathias Handeland <127216029+MathiasHandeland@users.noreply.github.com>
2025-08-22 08:46:43 +0200

    implemented a generic repository

--- api-cinema-challenge/api-cinema-challenge/Repository/IRepository.cs
+++ api-cinema-challenge/api-cinema-challenge/Repository/IRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace api_cinema_challenge.Repository
+﻿using System.Linq.Expressions;
+
+namespace api_cinema_challenge.Repository
 {
-    public interface IRepository
+    public interface IRepository<T>
     {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(int id);
+        Task<T> Delete(int id);
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+
+        Task<IEnumerable<T>> GetWithIncludes(params Expression<Func<T, object>>[] includes);
+
     }
 }
--- api-cinema-challenge/api-cinema-challenge/Repository/Repository.cs
+++ api-cinema-challenge/api-cinema-challenge/Repository/Repository.cs
@@ -1,6 +1,49 @@
-﻿namespace api_cinema_challenge.Repository
+﻿using api_cinema_challenge.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace api_cinema_challenge.Repository
 {
-    public class Repository
+    public class Repository<T> : IRepository<T> where T : class
     {
+        private CinemaContext _db;
+        private DbSet<T> _table = null!;
+
+        public Repository(CinemaContext db)
+        {
+            _db = db;
+            _table = db.Set<T>();
+        }
+
+
+        public async Task<T> Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await _table.ToListAsync();
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<T>> GetWithIncludes(params Expression<Func<T, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
