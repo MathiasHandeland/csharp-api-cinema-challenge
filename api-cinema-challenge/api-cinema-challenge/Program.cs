5e1dc8e5cd3d21661f11077f4dd3c199afa8a8c1
Mathias Handeland <127216029+MathiasHandeland@users.noreply.github.com>
2025-08-22 08:22:08 +0200

    updated program.cs

--- api-cinema-challenge/api-cinema-challenge/Program.cs
+++ api-cinema-challenge/api-cinema-challenge/Program.cs
@@ -1,20 +1,32 @@
 using api_cinema_challenge.Data;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<CinemaContext>();
+builder.Services.AddOpenApi();
+builder.Services.AddDbContext<CinemaContext>(options => {
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+    options.LogTo(message => Debug.WriteLine(message));
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "Demo API");
+    });
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
+
 app.Run();
